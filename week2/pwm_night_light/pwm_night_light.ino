9b8d9359cefc9a11e3ba85358be235cf9892b4b2
Puthiyaraj P <puthiyarajp49@gmail.com>
2026-07-04 18:47:52 +0530

    Add Q17 PWM Night Light

---
+++ week2/pwm_night_light/pwm_night_light.ino
@@ -0,0 +1,119 @@
+const int ledPin = 9;
+const int buttonPin = 2;
+
+int mode = 1;
+int lastButtonState = HIGH;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  Serial.println("Mode : Slow Breathing");
+}
+
+void checkButton()
+{
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW && lastButtonState == HIGH)
+  {
+    mode++;
+
+    if (mode > 3)
+      mode = 1;
+
+    if (mode == 1)
+      Serial.println("Mode : Slow Breathing");
+    else if (mode == 2)
+      Serial.println("Mode : Fast Pulse");
+    else
+      Serial.println("Mode : SOS");
+
+    delay(200);   // debounce
+  }
+
+  lastButtonState = buttonState;
+}
+
+void loop()
+{
+  checkButton();
+
+  if (mode == 1)
+  {
+    // Slow Breathing
+    for (int i = 0; i <= 255; i++)
+    {
+      analogWrite(ledPin, i);
+      delay(6);
+      checkButton();
+      if (mode != 1) return;
+    }
+
+    for (int i = 255; i >= 0; i--)
+    {
+      analogWrite(ledPin, i);
+      delay(6);
+      checkButton();
+      if (mode != 1) return;
+    }
+  }
+
+  else if (mode == 2)
+  {
+    // Fast Pulse
+    for (int i = 0; i <= 255; i++)
+    {
+      analogWrite(ledPin, i);
+      delay(1);
+      checkButton();
+      if (mode != 2) return;
+    }
+
+    for (int i = 255; i >= 0; i--)
+    {
+      analogWrite(ledPin, i);
+      delay(1);
+      checkButton();
+      if (mode != 2) return;
+    }
+  }
+
+  else if (mode == 3)
+  {
+    // SOS (... --- ...)
+    for (int i = 0; i < 3; i++)
+    {
+      digitalWrite(ledPin, HIGH);
+      delay(200);
+      digitalWrite(ledPin, LOW);
+      delay(200);
+      checkButton();
+      if (mode != 3) return;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+      digitalWrite(ledPin, HIGH);
+      delay(600);
+      digitalWrite(ledPin, LOW);
+      delay(200);
+      checkButton();
+      if (mode != 3) return;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+      digitalWrite(ledPin, HIGH);
+      delay(200);
+      digitalWrite(ledPin, LOW);
+      delay(200);
+      checkButton();
+      if (mode != 3) return;
+    }
+
+    delay(1000);
+  }
+}
